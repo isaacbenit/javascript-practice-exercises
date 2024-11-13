adaa3fe574587d7fa424c3b9ae5df373d438a780
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2024-11-13 14:57:40 +0200

    the odd and even exercise

---
+++ 01-fundamentals/01-conditionals/ex5 odd and even.js
@@ -0,0 +1,7 @@
+//Write a JavaScript for loop that iterates from 0 to 15. For each iteration, it checks if the current number is odd or even, and displays a message on the screen.
+
+for (let i=0; i<=15;i++){
+    if (i%2!==0){
+    console.log(i+'is odd');}
+    else{console.log(i+'is even')};
+}
