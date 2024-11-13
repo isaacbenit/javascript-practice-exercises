f6982085cd774fb8419093e36ba31c674b13b946
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2024-11-13 14:35:34 +0200

    the thrid and forth commit to day

---
+++ 01-fundamentals/01-conditionals/ex3 sort array.js
@@ -0,0 +1,3 @@
+//write a JavaScript conditional statement to sort three numbers. Display an alert box to show the results.
+let i=[-1,0,4]
+console.log(i.sort());
---
+++ 01-fundamentals/01-conditionals/ex4 greates in 5.js
@@ -0,0 +1,3 @@
+//Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the results.
+let a=[5, -2, -6, 0, -1]
+console.log(Math.max(...a));
