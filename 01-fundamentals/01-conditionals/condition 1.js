e9348683c30a051c76bee875c4ec17236d132546
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2024-11-13 11:44:46 +0200

    the second exercise

--- 01-fundamentals/01-conditionals/condition 1.js
+++ 01-fundamentals/01-conditionals/condition 1.js
@@ -1,6 +1,8 @@
-let a=15, b=12;
-if (a>b){
+
+//Write a JavaScript program that displays the largest integer among two integers.
+let a = 15, b = 12;
+if (a > b) {
     console.log('a is biger than b');
 }
-else{(console.log('b is greater than a'));};
+else { (console.log('b is greater than a')); };
 
---
+++ 01-fundamentals/01-conditionals/ex2 product.js
@@ -0,0 +1,5 @@
+//Write a JavaScript conditional statement to find the sign of the product of three numbers. Display an alert box with the specified sign.
+let a=3, b=-7, c=2;
+let d=a*b*c;
+if (d<0){console.log('number is negative');}
+else {console.log('number is positive')};
