c3f2b0b9815c5ff61e8d848c3edcc6f60d6f4735
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2024-12-18 09:34:18 +0200

    new kata

--- 01-fundamentals/codewars/addingCommars.js
+++ 01-fundamentals/codewars/addingCommars.js
@@ -1,6 +1,6 @@
 function commas(num){
     let p=[];
-     num = num.split('').reverse()
+     num = num.toString().split(',').join(' ').split('.').join(' ').reverse()
     for(i=0;i<num.length;i++){
         if((i%3)===0 && i>1){
         p.push(num[i]+',')
@@ -10,6 +10,6 @@ function commas(num){
     }
    return p.reverse().join('')
   }
-  console.log(commas('100000000000'))
+  console.log(commas(10,0.2,346))
 
  
