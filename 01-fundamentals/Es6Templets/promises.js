0fa625cd28340ee606b7ba19642b9fe3aa7a265c
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2024-12-19 13:36:45 +0200

    new lessons about es6

---
+++ 01-fundamentals/Es6Templets/promises.js
@@ -0,0 +1,11 @@
+function hello(){
+    return new Promise((resolve,reject)=>{
+        resolve('hello my g')
+    })
+}
+function bye(){
+    return new Promise((resolve,reject)=>{
+        resolve('ok byeeee👋🏽👋🏽')
+    })
+}
+hello().then(value=>{console.log(value); return bye()}).then(value=>console.log(value))
