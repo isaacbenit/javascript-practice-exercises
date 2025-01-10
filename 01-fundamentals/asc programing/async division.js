c000e503da181ff9af59d55151bb2eb1eaec4244
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-01-10 13:40:40 +0200

    new

--- 01-fundamentals/asc programing/async division.js
+++ 01-fundamentals/asc programing/async division.js
@@ -1,1 +1,15 @@
-function divide(di)
+function divide(disor,denominator){
+    new Promise ((resolve,reject)=>{
+        if (denominator === 0){
+            reject(`the denominator should not be equal to 0`)
+        }
+        else{
+            let c= disor/denominator
+            resolve(`the answer to ${disor} / ${denominator} = ${c} `)
+        }
+    })
+}
+async function show() {
+    const 
+    
+}
