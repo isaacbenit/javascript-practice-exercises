84469c9b6e0c54b6114e612f2629298161eaf13e
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-01-14 09:44:02 +0200

    new program in callbacks

--- 01-fundamentals/asc programing/fetching api.js
+++ 01-fundamentals/asc programing/fetching api.js
@@ -1,4 +1,21 @@
+setTimeout(()=>{
+    function check(){
+        return new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+              resolve('Success')
+              reject('failure') 
+            },2000)
+        })
+        
+    }
+    check().then((value => console.log(value)))
+    .catch( error => console.log(error))
+},0)
+
+
+
+
 fetch("https://jsonplaceholder.typicode.com/todos")
-.then(response=> response.json())
-.then(data=> console.log('it is fetched'))
-.catch(error => console.error(error))
+    .then(response=> response.json())
+    .then(data=> console.log(data.name))
+    .catch(error => console.error(error))
