c64ee7081e66024dd41640fe488d6fcc47b3b393
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-02-13 12:00:13 +0200

    new

---
+++ 01-fundamentals/codewars/Message Validator.js
@@ -0,0 +1,37 @@
+function isAValidMessage(message) {
+    let z = 0
+    if (isNaN(message[message.length - 1])) {
+        for (i = message.length - 1; i >= 0; i--) {
+            if (isNaN(message[i])) {
+                z += 1
+                console.log(z)
+            }
+            if (z < 0) {
+                return false
+            }
+            else if (!isNaN(message[i]) && !isNaN(message[i - 1])) {
+                let m = message[i - 1] + message[i]
+                z = z - m
+                i = i - 1
+            }
+            else {
+                z = z - message[i];
+            }
+        }
+        return z === 0 ? true : false;
+    }
+    else {
+        return false
+        // for(i=0;i<message.length;i++){
+        //     if(isNaN(message[i])){
+        //         p+=1 
+        //     }
+        //     else{
+        //         p=p-message[i];
+        //     }
+        // }
+        // return p===0?true:false;
+    }
+}
+console.log(isAValidMessage('4code13hellocodewars'))
+
