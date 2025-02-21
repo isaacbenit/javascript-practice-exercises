70c18ee283025f97158ba9943b0eaf14702e4748
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-02-21 16:05:23 +0200

    new changes

--- 01-fundamentals/codewars/Human readable duration format.js
+++ 01-fundamentals/codewars/Human readable duration format.js
@@ -5,8 +5,55 @@ function formatDuration (seconds) {
    if(seconds<3600){
     let minutes=(seconds/60).toFixed(0)
     seconds=seconds%60
-    return `${minutes} minutes  ${seconds} seconds`
+    if(minutes<1 && seconds==1){
+        return `${seconds} second` 
+    }
+    if(minutes<1){
+        return `${seconds} seconds` 
+    }
+    if(minutes==1 && seconds==1){
+       return `${minutes} minute  and ${seconds} second`
+    }
+    if(minutes>1 && seconds==0){
+        return `${minutes} minutes`
+    }
+    if(minutes==1 && seconds>1){
+        return `${minutes} minute and ${seconds} seconds`
+    }
+    if(minutes>1 && seconds==0){
+        return `${minutes} minutes`
+    }
+    return minutes===1?`${minutes} minute  ${seconds} seconds` :`${minutes} minutes  ${seconds} seconds`
    }
+   if(seconds>=3600){
+    let hours=(seconds/3600).toFixed(0)
+    seconds=seconds%3600
+    let minutes=(seconds/60).toFixed(0)
+    seconds=seconds%60
+    // console.log(hours==1)
+    if(hours==1 && minutes==1 && seconds==1){
+        return `${hours} hour, ${minutes} minute and ${seconds} second`
+    }
+    if(hours==1 && minutes==1 && seconds>1){
+        return `${hours} hour, ${minutes} minute and ${seconds} seconds`
+    }
+    if(minutes==1 && seconds==1 && hours>1){
+        return `${hours} hours, ${minutes} minute and ${seconds} second`
+    }
+    if(hours>1 && minutes==1 && seconds>1){
+        return `${hours} hours, ${minutes} minute and ${seconds} seconds`
+    }
+    if(hours>1 && minutes>1 && seconds==1){
+        return `${hours} hours, ${minutes} minutes and ${seconds} second`
+    }
+    if(hours==1 && seconds==0 && minutes == 0){
+        return `${hours} hour`
+    }
+    else{
+        return `${hours} hours, ${minutes} minutes and  ${seconds} seconds`
+    }
+
  
   }
-  console.log(formatDuration(62))
+}
+  console.log(formatDuration(120))
